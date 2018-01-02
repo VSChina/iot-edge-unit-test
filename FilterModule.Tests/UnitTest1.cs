6ed6e2a14d8fbb1fdc488fdbe66a5f861dab17a0
Jun Han <junhan@microsoft.com>
2018-01-02 16:44:39 +0800

    first commit

---
+++ FilterModule.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FilterModule;
+
+namespace FilterModule.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(Program.MethodToTest());
+        }
+    }
+}
